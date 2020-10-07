e85878afa4366aff3fe8da0aa265276524d55b10
ananin <sergioblancoxdd@gmail.com>
2020-10-07 09:04:55 +0200

    Nous fitxers

---
+++ index.php
@@ -0,0 +1,6 @@
+echo'Al gener, tanca la porta i encén el braser',
+echo'Al gener, tanca la porta i encén el braser';
+echo'Aigua de gener tot l'any va be';
+echo'Les gelades de gener fan florir l'ametller';
+echo'pel febrer, carnestoltes el primer';
+echo'mes tard o mes matiner carnaval dins el febrer';
